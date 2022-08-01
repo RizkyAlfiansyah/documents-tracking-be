2b0a506bbf5bb7328d783f3690268548e9c4be5e
Rizky Alfiansyah <rizkkyaf@gmail.com>
2022-08-01 20:09:37 +0800

    initial commit

---
+++ app/Http/Controllers/PrisonersController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Prisoners;
+use Illuminate\Http\Request;
+
+class PrisonersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Prisoners  $prisoners
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Prisoners $prisoners)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Prisoners  $prisoners
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Prisoners $prisoners)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Prisoners  $prisoners
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Prisoners $prisoners)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Prisoners  $prisoners
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Prisoners $prisoners)
+    {
+        //
+    }
+}
